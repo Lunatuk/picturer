ed7f39f09aba15501851752be33478f06b577f1e
Lunatuk <balancerd@yandex.ru>
2024-09-28 00:39:18 +0300

    V1.0 convertFileToHex

---
+++ src/src/FileProcessor.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileProcessor {
+
+    private static final String[] HEX_ARRAY = new String[256];
+
+    static {
+        for (int i = 0; i < 256; i++) {
+            HEX_ARRAY[i] = String.format("%02X", i);
+        }
+    }
+
+    public static void convertFileToHex(String filePath) throws IOException {
+        File hexString = new File("hexString.txt");
+        hexString.createNewFile();
+
+        try (FileOutputStream outputStream = new FileOutputStream(hexString);
+             FileInputStream fileInputStream = new FileInputStream(filePath)) {
+
+            byte[] buffer = new byte[10485760]; // буфер для чтения файла (10 MB)
+            int bytesRead;
+
+            StringBuilder hexBuffer = new StringBuilder();
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                for (int i = 0; i < bytesRead; i++) {
+                    hexBuffer.append(HEX_ARRAY[buffer[i] & 0xFF]);
+                }
+
+                outputStream.write(hexBuffer.toString().getBytes());
+                hexBuffer.setLength(0);
+            }
+        }
+    }
+}
